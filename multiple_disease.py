3f790e2bd24361fb2e663d25824f70636eec143b
Durgadevi Naveenkumar <durga.n3105@gmail.com>
2024-12-27 00:55:51 +0530

    Update multiple_disease.py

--- multiple_disease.py
+++ multiple_disease.py
@@ -6,11 +6,11 @@ import pickle
 import os
 
 # Function to connect to the PostgreSQL database
- def get_db_connection():
+def get_db_connection():
     conn = psycopg2.connect(
          host="dbdurga.c7igy8goq28e.ap-south-1.compute.amazonaws.com",
  )
- return conn
+return conn
 
 #Function to execute a query and return the result as a pandas DataFrame
 
