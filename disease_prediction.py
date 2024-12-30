948176b559f1e330ab0fb4b4cae2e38156d077a6
Durgadevi Naveenkumar <durga.n3105@gmail.com>
2024-12-31 03:05:56 +0530

    Update disease_prediction.py

--- disease_prediction.py
+++ disease_prediction.py
@@ -128,7 +128,7 @@ elif nav == "Liver Disease":
 
     # Load the liver's model
     try:
-        liver_model = pickle.load(open('XGBLiver.pkl', 'rb'))
+        liver_model = pickle.load(open('XGBliver.pkl', 'rb'))
     except FileNotFoundError:
         st.error("Model file not found. Please check the file path.")
         st.stop()    
