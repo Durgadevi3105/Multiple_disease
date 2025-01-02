f87381d26acdafd9228ade78f0d816c6735b950c
Durgadevi Naveenkumar <durga.n3105@gmail.com>
2025-01-02 10:48:18 +0530

    Update disease_prediction.py

--- disease_prediction.py
+++ disease_prediction.py
@@ -110,8 +110,6 @@ elif nav == "Kidney Disease":
                                 Serum_Creatinine,Sodium,Potassium,Hemoglobin,Packed_Cell_Volume,White_Blood_Cell_Count,Red_Blood_Cell_Count,
                                 1 if Hypertension == "yes" else 0,1 if Diabetes_Mellitus == "yes" else 0,1 if Coronary_Artery_Disease == "yes" else 0,
                                 1 if Appetite == "poor" else 0,1 if Pedal_Edema == "yes" else 0,1 if Anemia == "yes" else 0]]).astype(float)
-  
-    st.write(f"Input feature shape: {input_features.shape}")
     # Cleanse string columns (if necessary)
     for col in range(input_features.shape[1]):
         input_features[:, col] = [str(x).encode('utf-8').decode('utf-8') if isinstance(x, str) else x for x in input_features[:, col]]
